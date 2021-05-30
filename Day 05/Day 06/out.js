18bdff69521a756fb5cdc2e0bc2e4de482c6ca25
soojin08 <84217209+soojin08@users.noreply.github.com>
2021-05-30 09:15:15 +0900

    Add files via upload

---
+++ Day 05/Day 06/out.js
@@ -0,0 +1,6 @@
+function go() {
+    alert('go 함수를 누르셨군요!')
+}
+function move() {
+    alert('move 함수를 누르셨군요!')
+}
---
+++ Day 07/out.js
@@ -0,0 +1,15 @@
+function f1() {
+    alert('함수1을 사용(호출)하였음.')
+}
+
+function f2() {
+    alert('함수2을 사용(호출)하였음.')
+}
+
+function f3() {
+    alert('함수3을 사용(호출)하였음.')
+}
+
+function f4() {
+    alert('함수4을 사용(호출)하였음.')
+}
